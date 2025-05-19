56af3376e4782a214ca813648b03a266f9fd961e
1952153 <1952153@01-18104.pusd.dom>
2025-05-19 14:01:30 -0700

    Frame and Background Work

---
+++ DoodleJump/src/Background.java
@@ -0,0 +1,93 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
+
+public class Background {
+	private Image forward; 	
+	private AffineTransform tx;
+	
+	int width, height;
+	int x, y;						//position of the object
+	int vx, vy;						//movement variables
+	double scaleWidth = 4.0;		//change to scale image
+	double scaleHeight = 4.0; 		//change to scale image
+	
+	public Background() {
+		forward		= getImage("Clouds.jpg");
+		
+		
+		
+		width = 0;
+		height = 0;
+		
+		
+		
+		x = Frame.width/2 - width/2;
+		y = Frame.height - height*2;
+		
+		x = 0;
+		y = 0;
+		
+		vx = 0;
+		vy = 0 ;
+		
+		
+		tx = AffineTransform.getTranslateInstance(0, 0);
+
+		init(x, y); 				//initialize the location of the image
+									//use your variables
+	}
+	
+	
+	public Background(int x, int y) {
+		
+		this(); // invoke default constructor so it isn't copy pasted code
+		
+		// get object's attributes
+		// this: specifies x attribute instead of the parameter x
+		this.x = x;
+		this.y = y;
+		
+
+	}
+	
+	private void init(double a, double b) {
+		tx.setToTranslation(a, b);
+		tx.scale(scaleWidth, scaleHeight);
+	}
+	
+	public void paint(Graphics g) {
+		//these are the 2 lines of code needed draw an image on the screen
+		Graphics2D g2 = (Graphics2D) g;
+		
+		x+=vx;
+		y+=vy;	
+		
+		init(x,y);
+		
+		g2.drawImage(forward, tx, null);
+
+	}
+	
+	public void setCoord() {
+		
+		
+		
+	}
+
+	
+	private Image getImage(String path) {
+		Image tempImage = null;
+		try {
+			URL imageURL = Background.class.getResource(path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
+	
+}
---
+++ DoodleJump/src/Frame.java
@@ -0,0 +1,103 @@
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Frame extends JPanel implements ActionListener, MouseListener, KeyListener{
+	
+	// resolution of the frame	
+	static int width 		= 600;
+	static int height		= 900;
+	
+	public Frame() {
+		
+		JFrame frame = new JFrame("Doodle Jump");
+		frame.setSize(new Dimension(width, height));
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setUndecorated(false);
+		frame.setResizable(false);
+		frame.setTitle("Doodle Jump");
+		frame.addKeyListener(this);
+		
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}		
+	
+	public static void main(String[] args) {
+		
+		new Frame();
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
